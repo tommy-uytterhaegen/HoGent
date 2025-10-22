560d74f6b8856e8b152b61459b97f62d85140612
Tommy <tommy.uytterhaegen@gmail.com>
2025-10-22 13:44:33 +0200

    Init

---
+++ OefeningScheepvaart/OefeningScheepvaart/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OefeningScheepvaart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
