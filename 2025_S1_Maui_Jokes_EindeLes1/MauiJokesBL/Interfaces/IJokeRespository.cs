d102959bbf927ca61b0e4ae2fc5491688390e06a
Tommy <tommy.uytterhaegen@gmail.com>
2025-11-27 20:47:42 +0100

    Initial

---
+++ 2025_S1_Maui_Jokes_EindeLes1/MauiJokesBL/Interfaces/IJokeRespository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoGentMauiBL.Interfaces
+{
+    public interface IJokeRespository
+    {
+        void Add(string joke);
+        bool Exists(string joke);
+        string Get(int jokeIndex);
+        int GetCount();
+    }
+}
