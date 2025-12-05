89849f44ffb07958250eee91f9574f2437e0770e
Tommy <tommy.uytterhaegen@gmail.com>
2025-12-05 08:35:02 +0100

    Initial Commit of 'Einde les 2'

---
+++ 2025_S1_Maui_Jokes_EindeLes2/MauiJokes/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using HoGentMauiBL.Services;
+using HoGentMauiDL;
+
+namespace HoGentMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        private JokeService JokeService { get; init; }
+
+        public MainPage(JokeService jokeService)
+        {
+            InitializeComponent();
+
+            JokeService = jokeService;
+        }
+
+        private void RandomJokeButton_Clicked(object sender, EventArgs e)
+        {
+            JokeLabel.Text = JokeService.GetRandomJoke();
+        }
+
+        private void AddJokeButton_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(NewJokeEntry.Text))
+            {
+                JokeService.AddJoke(NewJokeEntry.Text);
+
+                NewJokeEntry.Text = null;
+
+                DisplayAlert("Toegevoegd", "De grap is toegevoegd", "Sluiten");
+            }
+            else
+                DisplayAlert("Gefaald", "Er is geen grap ingevoerd", "Sluiten");
+        }
+
+        private void DeleteJokeButton_Clicked(object sender, EventArgs e)
+        {
+            var isDeleted = JokeService.DeleteJoke(JokeLabel.Text);
+            if (isDeleted)
+            {
+                DisplayAlert("Verwijderd", "De grap is verwijderd", "Sluiten");
+                JokeLabel.Text = "";
+            }
+            else
+                DisplayAlert("Gefaald", "De grap is NIET verwijderd", "Sluiten");
+        }
+    }
+}
